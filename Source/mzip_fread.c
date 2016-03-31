04beeae77a4d6f7d5ae170850fe0dfa97c0d09f7
anhuichen1992 <gordon@chenGuanglongdeMacBook-Pro.local>
2016-04-01 00:14:27 +0800

    实现了使用mmap()来进行拷贝。

--- Source/mzip_fread.c
+++ Source/mzip_fread.c
@@ -6,38 +6,37 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#define READ_SIZE ((off_t)1*getpagesize())
 #endif
 
 #if defined(WINDOWS)
 #define off_t off64_t
 #define fseeko fseeko64
 #define ftello ftello64
+#define READ_SIZE ((off_t)1*4096)
 #endif
 
-//#define SECTION_SIZE ((off_t)4*1024*1024*1024)
-off_t SECTION_SIZE;
-#define READ_SIZE ((off_t)1*getpagesize())
-
-int send_zip(char *name);
-int recv_zip(char *name);
+int send_zip(char *name, off_t section_size);
+int recv_zip(char *name, off_t section_size);
 int copy_file(char *to, off_t to_location, char *name, off_t from_location, off_t size);
-int chg_section_num(off_t file_size);
+int crt_section_num(off_t file_size, off_t section_size);
 int get_section_num(char *name);
 off_t get_file_size(char *name);
 
 int main(int argc, char *argv[])
 {
+    int num;
+    off_t section_size;
+    if(argc >= 4 && (num = atoi(argv[3])) && num > 0 && num <= 4) {
+        section_size = ((off_t)num*1024*1024*1024);
+    } else {
+        section_size = ((off_t)3*1024*1024*1024);
+    }
     if(argc >= 3) {
         if(!strcmp(argv[1], "-s")) {
-            int num;
-            if(argc >= 4 && (num = atoi(argv[3])) && num > 0 && num <= 4) {
-                SECTION_SIZE = ((off_t)num*1024*1024*1024);
-            } else {
-                SECTION_SIZE = ((off_t)4*1024*1024*1024);
-            }
-            return send_zip(argv[2]);
+            return send_zip(argv[2], section_size);
         } else if(!strcmp(argv[1], "-r")) {
-            return recv_zip(argv[2]);
+            return recv_zip(argv[2], section_size);
         }
     }
 
@@ -47,7 +46,7 @@ int main(int argc, char *argv[])
 
 }
 
-int send_zip(char *name)
+int send_zip(char *name, off_t section_size)
 {
     int res = 0;
     char *des_name;
@@ -65,7 +64,7 @@ int send_zip(char *name)
     }
     printf("File:%s, Size:%lldB\n", name, file_size);
 
-    int section_num = chg_section_num(file_size);
+    int section_num = crt_section_num(file_size, section_size);
     if(section_num < 0) {
         free(des_name);
         return -1;
@@ -77,9 +76,9 @@ int send_zip(char *name)
     for(i = 0; i < section_num; i++) {
         des_name[des_name_len] = '0' + i;
         remove(des_name);
-        location = SECTION_SIZE * i;
-        if(SECTION_SIZE * (i + 1) <= file_size) {
-            res = copy_file(des_name, 0, name, location, SECTION_SIZE);
+        location = section_size * i;
+        if(section_size * (i + 1) <= file_size) {
+            res = copy_file(des_name, 0, name, location, section_size);
         } else {
             res = copy_file(des_name, 0, name, location, file_size - location);
         }
@@ -156,43 +155,49 @@ int copy_file(char *to, off_t to_location, char *from, off_t from_location, off_
         close(from_fd);
         return -1;
     }
-    off_t to_size = get_file_size(to);
-    ftruncate(to_fd, to_size + size);
+    ftruncate(to_fd, to_location + size);
 
     char *from_mm, *to_mm;
-    int i = 0;
+    int num_max = 1*1024*1024*1024 / READ_SIZE;
+    int num_sum = 0;
     while(size >= READ_SIZE) {
-        from_mm = mmap(NULL, READ_SIZE, PROT_READ, MAP_PRIVATE, from_fd, i * READ_SIZE);
+        int num = size / READ_SIZE;
+        if(num > num_max) {
+            num = num_max;
+        }
+        from_mm = mmap(NULL, num * READ_SIZE, PROT_READ, MAP_PRIVATE, from_fd, from_location + num_sum * READ_SIZE);
         if(from_mm == MAP_FAILED) {
-            perror("mmap() from_fd failed:");
+            perror("mmap() from_file failed");
             free(buf);
             close(from_fd);
             close(to_fd);
             return -1;
         }
-        to_mm = mmap(NULL, READ_SIZE, PROT_WRITE, MAP_SHARED, to_fd, to_size);
+        to_mm = mmap(NULL, num * READ_SIZE, PROT_WRITE, MAP_SHARED, to_fd, to_location + num_sum * READ_SIZE);
         if(to_mm == MAP_FAILED) {
-            perror("mmap() to_fd failed:");
+            perror("mmap() to_file failed");
             free(buf);
-            munmap(from_mm, READ_SIZE);
+            munmap(from_mm, num * READ_SIZE);
             close(from_fd);
             close(to_fd);
             return -1;
         }
-        memcpy(to_mm, from_mm, READ_SIZE);
-        i++;
-        size -= READ_SIZE;
-        to_size += READ_SIZE;
-        munmap(from_mm, READ_SIZE);
-        munmap(to_mm, READ_SIZE);
+        memcpy(to_mm, from_mm, num * READ_SIZE);
+        num_sum += num;
+        size -= num * READ_SIZE;
+        munmap(from_mm, num * READ_SIZE);
+        munmap(to_mm, num * READ_SIZE);
     }
-    lseek(from_fd, i * READ_SIZE, SEEK_SET);
-    lseek(to_fd, to_size, SEEK_SET);
+    lseek(from_fd, from_location + num_sum * READ_SIZE, SEEK_SET);
+    lseek(to_fd, to_location + num_sum * READ_SIZE, SEEK_SET);
     off_t len;
-    if(size >= 1 && (len = read(from_fd, buf, READ_SIZE)) > 0 && write(to_fd, buf, len) > 0) {
-        ;
-    } else {
-         res = -1;
+    if(size >= 1) {
+        if((len = read(from_fd, buf, READ_SIZE)) > 0 && write(to_fd, buf, len) > 0) {
+            ;
+        } else {
+            perror("tail read() or write() failed");
+            res = -1;
+        }
     }
     close(from_fd);
     close(to_fd);
@@ -201,7 +206,7 @@ int copy_file(char *to, off_t to_location, char *from, off_t from_location, off_
     return res;
 }
 
-int recv_zip(char *name)
+int recv_zip(char *name, off_t section_size)
 {
     int res = 0;
     char *recv_name;
@@ -223,7 +228,7 @@ int recv_zip(char *name)
     remove(recv_name);
     for(i = 0; i < section_num; i++) {
         name[strlen(name) - 1] = '0' + i;
-        if(copy_file(recv_name, i * SECTION_SIZE, name, 0, get_file_size(name)) < 0) {
+        if(copy_file(recv_name, i * section_size, name, 0, get_file_size(name)) < 0) {
             res = -1;
             break;
         }
@@ -238,10 +243,10 @@ int recv_zip(char *name)
     return res;
 }
 
-int chg_section_num(off_t file_size)
+int crt_section_num(off_t file_size, off_t section_size)
 {
-    int section_num = file_size / SECTION_SIZE;
-    section_num += (section_num * SECTION_SIZE < file_size ? 1 : 0);
+    int section_num = file_size / section_size;
+    section_num += (section_num * section_size < file_size ? 1 : 0);
     if(section_num > 9) {
         return -1;
     }
